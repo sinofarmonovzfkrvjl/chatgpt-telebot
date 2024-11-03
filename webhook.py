be99a033ec0d675aa180754892d310a0397ff681
sinofarmonovzfkrvjl <farmonovsino4@gmail.com>
2024-11-03 12:41:07 -0800

    updated

--- webhook.py
+++ webhook.py
@@ -4,7 +4,7 @@ import requests
 from flask import Flask, request
 from main import bot, API_TOKEN, types  # Import the function to process updates
 
-WEBHOOK_URL = "https://uzbekistan-weather-telebot-private.onrender.com/webhook"  # Replace with your ngrok URL
+WEBHOOK_URL = "https://chatgpt-telebot-zfkrvjl323.onrender.com/webhook"  # Replace with your ngrok URL
 
 # Set up Flask app
 app = Flask(__name__)
